755f6532abd850a698c2c6d25d6d1414e7b62212
Dulaj Atapattu <dulaj.atapattu@h2o.ai>
2020-11-16 19:03:55 +0530

    Remove null checks

--- src/churn_predictor.py
+++ src/churn_predictor.py
@@ -32,16 +32,12 @@ class ChurnPredictor:
         self.test_df = h2o.import_file(path=testing_data_path, destination_frame='telco_churn_test.csv')
 
     def predict(self):
-        if not self.test_df:
-            print('No test data frame is set')
         self.predicted_df = self.gbm.predict(self.test_df)
 
     def get_churn_rate_of_customer(self, customer_no):
         pass
 
     def get_shap_explanation(self, row_index):
-        if not self.test_df:
-            print('No test data frame is set')
         return self.gbm.shap_explain_row_plot(frame=self.test_df, row_index=row_index, figsize=[20,6])
 
     def get_partial_dependence_explanation(self, customer_no, feature):
