8c7f69475781d432706607316ba7d4811e1327dc
VijithaEkanayake <ekvijitha@gmail.com>
2020-11-17 01:26:24 +0530

    Remove dash borad page and home content

--- src/app.py
+++ src/app.py
@@ -1,63 +1,13 @@
 from h2o_wave import Q, ui, app, main
 
 import pandas as pd
-import numpy as np
-from .utils import ui_table_from_df, python_code_content
-from .plots import html_hist_of_target_percent, html_map_of_target_percent, html_pie_of_target_percent, wide_stat_card_dollars, tall_stat_card_dollars, get_image_from_matplotlib
+from .utils import python_code_content
+from .plots import html_pie_of_target_percent, wide_stat_card_dollars, tall_stat_card_dollars, get_image_from_matplotlib
 from .config import Configuration
 from .churn_predictor import ChurnPredictor
 
 config = Configuration()
 churn_predictor = ChurnPredictor()
-
-
-def home_content(training_file_url, header_png):
-    df = pd.read_csv(config.working_data).head()
-
-    items = [
-        ui.text_xl('User Guide'),
-        ui.frame(content=' ', height="20px"),  # whitespace
-
-        ui.text(f'![{config.y_col}]({header_png})'),
-        ui.frame(content=' ', height="20px"),  # whitespace
-
-        ui.text('''This Churn Prediction application allows for exploring historical churn data, building a predictive 
-        churn model, and exploring those predictions. Dashboards are available to understand both global trends and 
-        the behavior of each customer.'''),
-        ui.frame(content=' ', height="20px"),  # whitespace
-
-        ui.separator("Data"),
-        ui.frame(content=' ', height="20px"),  # whitespace
-
-        ui.text(f'The historic training data comes packaged with this application. You can find out more about it from '
-                f'[this Kaggle page](https://www.kaggle.com/c/churn-analytics-bda/overview). Click '
-                f'[here]({training_file_url}) to download the training dataset.'),
-        ui.frame(content=' ', height="20px"),  # whitespace
-
-        ui_table_from_df(df, 5, "Training Data")
-
-    ]
-
-    return items
-
-
-# TODO currently 3 cols hard coded, make this more general
-def dashboard_content():
-
-    column_name = config.get_column_type()
-
-    items = [
-        ui.text_xl(f'Summary of Churn on {config.get_analysis_type()}'),
-        ui.frame(content=html_map_of_target_percent(config.working_data, column_name, "State", config.color), height='60%'),
-        ui.frame(content=html_hist_of_target_percent(config.working_data, column_name, "Total_Intl_Charge", config.color),
-                 height='60%'),
-        ui.frame(content=html_hist_of_target_percent(config.working_data, column_name, "Account_Length", config.color),
-                 height='60%'),
-
-    ]
-
-    return items
-
 
 def profile_content():
     df = pd.read_csv(config.testing_data_url).head(40)
@@ -72,33 +22,12 @@ def profile_content():
     ]
     return items
 
-
 async def profile_selected_page(q: Q):
     del q.page["content"]
     df = pd.read_csv(config.testing_data_url)
 
-    # q.page["content"].items = [ui.text_xl(content="Values and Percentiles for Customer: " + str(q.args.customers[0]))]
-    # print(df[q.args.customers['Churn?']])
     cust_phone_no = q.args.customers[0]
     q.client.selected_customer_index = int(df[df[config.id_column] == cust_phone_no].index[0])
-
-    if config.model_loaded:
-        churn_pct = df[df[config.id_column] == cust_phone_no]['Churn.1'].values[0]
-
-        if churn_pct > 0.5:
-            color = '$red'
-        else:
-            color = '$green'
-
-        q.page['churn_stat'] = ui.tall_gauge_stat_card(
-            box='4 4 1 2',
-            title="Likelihood to Churn",
-            value='={{intl foo style="percent" minimum_fraction_digits=2 maximum_fraction_digits=2}}',
-            aux_value='',
-            plot_color=color,
-            progress=churn_pct,
-            data=dict(foo=churn_pct, bar=0),
-        )
 
     df['Total Charges'] = df.Total_Day_charge + df.Total_Eve_Charge + df.Total_Night_Charge + df.Total_Intl_Charge
     df = df[["Total_Day_charge", "Total_Eve_Charge", "Total_Night_Charge", "Total_Intl_Charge", config.id_column,
@@ -147,7 +76,6 @@ async def profile_selected_page(q: Q):
         image=get_image_from_matplotlib(top_positive_pd_plot),
     )
 
-
 async def initialize_page(q: Q):
 
     content = []
@@ -159,7 +87,7 @@ async def initialize_page(q: Q):
 
         q.app.header_png, = await q.site.upload([config.image_path])
         q.app.training_file_url, = await q.site.upload([config.working_data])
-        content = home_content(q.app.training_file_url, q.app.header_png)
+        content = profile_content()
         q.client.app_initialized = True
 
     q.page.drop()
@@ -175,8 +103,6 @@ async def initialize_page(q: Q):
     q.page['side_bar'] = ui.tab_card(
         box='4 1 -1 1',
         items=[
-            ui.tab(name='#home', label='Home'),
-            ui.tab(name='#dashboard', label='Global Dashboard'),
             ui.tab(name='#profile', label='Customer Profiles'),
             ui.tab(name='#tour', label='Application Code'),
         ],
@@ -188,7 +114,6 @@ async def initialize_page(q: Q):
 
     await q.page.save()
 
-
 @app('/')
 async def serve(q: Q):
 
@@ -198,33 +123,10 @@ async def serve(q: Q):
     if q.args.select_customer_button:
         await profile_selected_page(q)
 
-    elif q.args.select_model_button:
-        content.items = [ui.progress(label="Making Predictions in Driverless AI")]
-        await q.page.save()
-
-        predictions_file_url, = await q.site.upload([config.working_data])
-
-        df = pd.read_csv(config.working_data)
-
-        content.items = [
-            ui.text_xl(f"{config.y_col} Predictions have been Made on New Data"),
-            ui.frame(content=' ', height="20px"),  # whitespace
-            ui.text(
-                f"See the Dashboard and Customer Profiles for the results! "
-                f"[Download the model predictions]({predictions_file_url})."),
-            # ui_table_from_df(df, 10, 'predictions')
-        ]
-
     else:
         hash = q.args['#']
 
-        if hash == 'home':
-            content.items = home_content(q.app.training_file_url, q.app.header_png)
-
-        elif hash == 'dashboard':
-            content.items = dashboard_content()
-
-        elif hash == 'profile':
+        if hash == 'profile':
             content.items = profile_content()
 
         elif hash == 'tour':
