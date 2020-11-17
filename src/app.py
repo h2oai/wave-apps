cc966fb6ead49fb595cb0429587ce8b123568073
VijithaEkanayake <ekvijitha@gmail.com>
2020-11-17 08:39:47 +0530

    Rearrange the code

--- src/app.py
+++ src/app.py
@@ -29,19 +29,59 @@ async def profile_selected_page(q: Q):
     cust_phone_no = q.args.customers[0]
     q.client.selected_customer_index = int(df[df[config.id_column] == cust_phone_no].index[0])
 
+    await populate_customer_churn_stats(cust_phone_no, df, q)
+
+    await populate_churn_plots(q)
+
+
+async def populate_churn_plots(q):
+    shap_plot = churn_predictor.get_shap_explanation(q.client.selected_customer_index)
+    q.page['shap_plot'] = ui.image_card(
+        box=config.boxes['shap_plot'],
+        title='',
+        type='png',
+        image=get_image_from_matplotlib(shap_plot),
+    )
+    top_negative_pd_plot = churn_predictor.get_top_negative_pd_explanation(q.client.selected_customer_index)
+    q.page['top_negative_pd_plot'] = ui.image_card(
+        box=config.boxes['top_negative_pd_plot'],
+        title='',
+        type='png',
+        image=get_image_from_matplotlib(top_negative_pd_plot),
+    )
+    top_positive_pd_plot = churn_predictor.get_top_positive_pd_explanation(q.client.selected_customer_index)
+    q.page['top_positive_pd_plot'] = ui.image_card(
+        box=config.boxes['top_positive_pd_plot'],
+        title='',
+        type='png',
+        image=get_image_from_matplotlib(top_positive_pd_plot),
+    )
+
+
+async def populate_customer_churn_stats(cust_phone_no, df, q):
+
     df['Total Charges'] = df.Total_Day_charge + df.Total_Eve_Charge + df.Total_Night_Charge + df.Total_Intl_Charge
+
     df = df[["Total_Day_charge", "Total_Eve_Charge", "Total_Night_Charge", "Total_Intl_Charge", config.id_column,
              "Total Charges"]]
-    df.columns = ["Day Charges", "Evening Charges", "Night Charges", "Int'l Charges", config.id_column, "Total Charges"]
-    q.page["day_stat"]  = wide_stat_card_dollars(df, cust_phone_no, "Day Charges", config.boxes['day_stat'], config.color)
-    q.page["eve_stat"] = wide_stat_card_dollars(df, cust_phone_no, "Evening Charges", config.boxes['eve_stat'], config.color)
-    q.page["night_stat"] = wide_stat_card_dollars(df, cust_phone_no, "Night Charges", config.boxes['night_stat'], config.color)
-    q.page["intl_stat"] = wide_stat_card_dollars(df, cust_phone_no, "Int'l Charges", config.boxes['intl_stat'], config.color)
-    q.page["total_stat"] = tall_stat_card_dollars(df, cust_phone_no, "Total Charges", config.boxes['total_stat'], config.total_gauge_color)
 
+    df.columns = ["Day Charges", "Evening Charges", "Night Charges", "Int'l Charges", config.id_column, "Total Charges"]
+
+    q.page["day_stat"] = wide_stat_card_dollars(df, cust_phone_no, "Day Charges", config.boxes['day_stat'],
+                                                config.color)
+    q.page["eve_stat"] = wide_stat_card_dollars(df, cust_phone_no, "Evening Charges", config.boxes['eve_stat'],
+                                                config.color)
+    q.page["night_stat"] = wide_stat_card_dollars(df, cust_phone_no, "Night Charges", config.boxes['night_stat'],
+                                                  config.color)
+    q.page["intl_stat"] = wide_stat_card_dollars(df, cust_phone_no, "Int'l Charges", config.boxes['intl_stat'],
+                                                 config.color)
+    q.page["total_stat"] = tall_stat_card_dollars(df, cust_phone_no, "Total Charges", config.boxes['total_stat'],
+                                                  config.total_gauge_color)
     q.page['customer'] = ui.small_stat_card(box=config.boxes['customer'], title='Customer', value=str(cust_phone_no))
+
     q.page['churn_rate'] = ui.small_stat_card(box=config.boxes['churn_rate'], title='Churn Rate',
-                                              value=str(churn_predictor.get_churn_rate_of_customer(q.client.selected_customer_index)) + ' %')
+                                              value=str(churn_predictor.get_churn_rate_of_customer(
+                                                  q.client.selected_customer_index)) + ' %')
 
     labels = ["Day Charges", "Evening Charges", "Night Charges", "Int'l Charges"]
     values = [df[df[config.id_column] == cust_phone_no][labels[0]].values[0],
@@ -50,31 +90,8 @@ async def profile_selected_page(q: Q):
               df[df[config.id_column] == cust_phone_no][labels[3]].values[0]]
 
     q.page['stat_pie'] = ui.frame_card(box=config.boxes['stat_pie'], title='Total call charges breakdown',
-        content=html_pie_of_target_percent('', labels,values))
+                                       content=html_pie_of_target_percent('', labels, values))
 
-    shap_plot = churn_predictor.get_shap_explanation(q.client.selected_customer_index)
-    q.page['shap_plot'] = ui.image_card(
-        box=config.boxes['shap_plot'],
-        title='',
-        type='png',
-        image=get_image_from_matplotlib(shap_plot),
-    )
-
-    top_negative_pd_plot = churn_predictor.get_top_negative_pd_explanation(q.client.selected_customer_index)
-    q.page['top_negative_pd_plot'] = ui.image_card(
-        box=config.boxes['top_negative_pd_plot'],
-        title='',
-        type='png',
-        image=get_image_from_matplotlib(top_negative_pd_plot),
-    )
-
-    top_positive_pd_plot = churn_predictor.get_top_positive_pd_explanation(q.client.selected_customer_index)
-    q.page['top_positive_pd_plot'] = ui.image_card(
-        box=config.boxes['top_positive_pd_plot'],
-        title='',
-        type='png',
-        image=get_image_from_matplotlib(top_positive_pd_plot),
-    )
 
 async def initialize_page(q: Q):
 
