5ee79f8814903ebcac20c2c9b5fb7232f55acb3e
VijithaEkanayake <ekvijitha@gmail.com>
2020-11-30 12:59:46 +0530

    Initial twitter sentiment implementation

---
+++ twitter-sentiment/src/app.py
@@ -0,0 +1,130 @@
+from h2o_wave import Q, site, app, ui, main
+
+import tweepy
+import json
+import operator
+import itertools
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from .config import Configuration
+
+config = Configuration()
+
+configuration = {
+    'access_token'  : '955862386388344832-7ZYvXCkpyFO6Pbhh93Od4O94E5ga1t4', 
+    'access_token_secret' : 'OfRg3O4SnrOinda7VeOTjfIBfWe9uzmRJd5IBEsQOdvvl', 
+    'api_key' : 'c3QdMOOWQFHTsI28Yu3HGjwkO', 
+    'api_secret_key' : 'V1FwzrxBzr7aYQNeqQf4s6D1oBapwpIVNtSkceJOXtnLKemAOS'}
+
+auth = tweepy.OAuthHandler(configuration['api_key'], configuration['api_secret_key'])
+auth.set_access_token(configuration['access_token'], configuration['access_token_secret'])
+
+analyser = SentimentIntensityAnalyzer()
+
+api = tweepy.API(auth)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+def texts(tag):
+    tws = {}
+    texts = []
+    cc = 0
+    # for tweet in api.search(q=tag, lang="en", rpp=100).items(MAX_TWEETS):
+    for tweet in tweepy.Cursor(api.search, q=tag, lang="en", rpp=100).items(16):
+        if (not tweet.retweeted) and cc < 25: # and ('RT @' not in tweet.text):
+            texts.append(tweet.text)
+            cc = cc+1
+        else:
+            break
+    
+    texts = list(set(texts))
+    for t in texts:
+        score = analyser.polarity_scores(t)
+        sc = score.copy()
+        sc.pop('compound', None)
+        if sc['pos'] < sc['neg']:
+            m = 'neg'
+        else:
+            m = 'pos'
+        mm = {'neu': 'Neutral', 'pos': 'Postive', 'neg': 'Negative'}
+        tws[t] = (mm[m], sc)
+    return (tws, texts)
+
+
+def home_content(q: Q):
+    q.page.add("search_tab", ui.form_card(box=config.boxes["search_tab"], items=[
+        ui.textbox(name='text',
+                   label='',
+                   placeholder='#h2oai',
+                   value=q.args.text, multiline=False, trigger=False)]))
+
+    q.page.add("search_click", ui.form_card(box=config.boxes["search_click"], items=[
+        # ui.Buttons(
+        ui.button(name="search", label="search", primary=True)]))
+
+async def initialize_page(q: Q):
+
+    if not q.client.initalized:
+        q.args.text = 'AI'
+        q.args.search = True
+        q.client.initalized = True
+        (q.app.header_png,) = await q.site.upload([config.image_path])
+        q.client.app_initialized = True
+
+    q.page.drop()
+
+    q.page["title"] = ui.header_card(
+        box=config.boxes["banner"],
+        title=config.title,
+        subtitle=config.subtitle,
+        icon=config.icon,
+        icon_color=config.color,
+    )
+
+    q.page["nav_bar"] = ui.form_card(
+        box=config.boxes["navbar"],
+        items=[],
+    )
+
+    home_content(q)
+
+
+async def list_tweets_for_hashtag(q):
+    q.user.text = q.args.text
+    values, text = texts(q.user.text)
+    cc = 0
+    boxes = [' '.join(i) for i in list(itertools.product(['3', '5', '7', '9', '11'], ['1', '4', '7', '10']))]
+    for t in text:
+        print(t)
+        val = values[t]
+        if val[0] == 'Negative':
+            color = '$red'
+        elif val[0] == 'Positive':
+            color = '$blue'
+        else:
+            color = '$green'
+
+        j, i = boxes[cc].split(' ')
+        width = 3 if not i == '10' else '-1'
+        q.page.add(f'example_{i}_{j}', ui.large_bar_stat_card(
+            box=f'{i} {j} {width} 2',
+            title=f'Sentiment - {val[0]}',
+            value='={{intl foo minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+            value_caption='Negative',
+            aux_value='={{intl bar minimum_fraction_digits=2 maximum_fraction_digits=2}}',
+            aux_value_caption='Positive',
+            plot_color=color,
+            progress=1,
+            data=dict(foo=val[1]['neg'], bar=val[1]['pos']),
+            caption=t,
+        ))
+        cc = cc + 1
+
+
+@app('/')
+async def serve(q: Q):
+
+    await initialize_page(q)
+    if q.args.search:
+        await list_tweets_for_hashtag(q)
+
+    await q.page.save()
---
+++ twitter-sentiment/src/config.py
@@ -0,0 +1,24 @@
+class Configuration:
+    """
+    Configuration file for Telco Customer Churn
+    """
+
+    def __init__(self):
+        self.color = "#00A8E0"
+        self.total_gauge_color = "#FF0102"
+        self.image_path = "static/icon.jpeg"
+
+        self.default_model = "twitter_sentiment_model"
+
+        self.title = "Twitter Sentiment"
+        self.subtitle = "Searches twitter hashtags and sentiment analysis"
+        self.icon = "UpgradeAnalysis"
+
+
+        self.boxes = {
+            "banner": "1 1 3 1",
+            "logo": "11 1 -1 1",
+            "navbar": "4 1 -1 1",
+            "search_click":"10 2 -1 1",
+            "search_tab": "1 2 9 1"
+        }
