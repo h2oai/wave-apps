467c0382936e8db28ed018fae47e371f5283da9a
Dulaj Atapattu <dulaj.atapattu@h2o.ai>
2020-11-30 21:54:16 +0530

    Removed unused code

--- shopping-cart-recommendations/src/app.py
+++ shopping-cart-recommendations/src/app.py
@@ -22,48 +22,6 @@ def get_product_mapping() -> dict:
     return product_mapping
 
 
-def get_profitability_bucket(profitability: float) -> str:
-    """
-    Profitability bucket.
-    """
-    if profitability >= 0.25:
-        return "Very High"
-    elif profitability >= 0.20:
-        return "High"
-    elif profitability >= 0.10:
-        return "Medium"
-    elif profitability >= 0.05:
-        return "Low"
-    else:
-        return "Very Low"
-
-
-def get_diversity_bucket(diversity: float) -> str:
-    """
-    Diversity bucket.
-    """
-    if diversity >= 0.99:
-        return "Full"
-    elif diversity >= 0.51:
-        return "High"
-    elif diversity >= 0.49:
-        return "Balanced"
-    elif diversity > 0.0:
-        return "Low"
-    else:
-        return "None"
-
-
-def get_diversity_color(diversity: float) -> str:
-    """
-    Color of diversity stat.
-    """
-    if diversity in [0.0, 1.0]:
-        return 'green'
-    else:
-        return 'orange'
-
-
 async def initialize_app(q: Q):
     q.client.rulesets = pd.read_csv('data/instacart_market_basket_model.csv').sort_values('profitability',
                                                                                           ascending=False)
@@ -81,46 +39,6 @@ async def initialize_app(q: Q):
         icon=config.icon,
         icon_color=config.icon_color,
     ))
-
-    await update_cart(q, add_product=False)
-
-
-async def update_cart(q: Q, add_product: bool = True):
-    if add_product:
-        if q.client.suggested_product_1:
-            new_product = q.client.suggested_products[0]
-        elif q.client.suggested_product_2:
-            new_product = q.client.suggested_products[1]
-        elif q.client.suggested_product_3:
-            new_product = q.client.suggested_products[2]
-        else:
-            new_product = []
-
-        q.client.cart_products += [new_product]
-
-        q.client.suggested_products = q.client.df[
-                                          q.client.df.antecedents.str.contains(f"'{new_product}'")
-                                      ].consequents.values[:3]
-
-    q.page['cart'].items[2].picker.values = q.client.cart_products
-
-    q.page['suggestions'].items[1].button.visible = False
-    q.page['suggestions'].items[2].button.visible = False
-    q.page['suggestions'].items[3].button.visible = False
-
-    if len(q.client.suggested_products) > 0:
-        q.page['suggestions'].items[1].button.visible = True
-        q.page['suggestions'].items[1].button.label = q.client.suggested_products[0]
-
-    if len(q.client.suggested_products) > 1:
-        q.page['suggestions'].items[2].button.visible = True
-        q.page['suggestions'].items[2].button.label = q.client.suggested_products[1]
-
-    if len(q.client.suggested_products) > 2:
-        q.page['suggestions'].items[3].button.visible = True
-        q.page['suggestions'].items[3].button.label = q.client.suggested_products[2]
-
-    await q.page.save()
 
 
 def get_suggestions(q: Q, cart_products, count=3):
@@ -172,9 +90,6 @@ def render_suggestions(q: Q):
 
 @app('/')
 async def serve(q: Q):
-    """
-    Main serving function.
-    """
     if not q.client.initialized:
         await initialize_app(q)
         q.client.initialized = True
