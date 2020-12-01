b682e93f107e6812f7e8c19122d36031b5fc5103
Dulaj Atapattu <dulaj.atapattu@h2o.ai>
2020-12-01 17:23:08 +0530

    Added py doc comments

--- shopping-cart-recommendations/src/utils.py
+++ shopping-cart-recommendations/src/utils.py
@@ -9,6 +9,14 @@ def get_products_list():
 
 
 def get_suggestions(df, cart_products, count=3):
+    """
+    Get product suggestions based on the products already in the cart.
+
+    :param df: Pandas dataframe of market basket analysis rule set
+    :param cart_products: list of products in the cart
+    :param count: no of suggestions needed
+    :return: list of strings of suggested product names
+    """
     suggestions = pd.DataFrame(columns=df.columns)
 
     for product in cart_products:
@@ -20,6 +28,14 @@ def get_suggestions(df, cart_products, count=3):
 
 
 def get_trending_products(df, cart_products, count=5):
+    """
+    Get trending products at the moment. This is not user/order specific.
+
+    :param df: Pandas dataframe of market basket analysis rule set
+    :param cart_products: list of products in the cart. This needed to skip same products in the output.
+    :param count: no of trending products needed
+    :return: list of strings of trending product names
+    """
     # TODO: Add a proper logic to get trending products
     df = df[is_not_in_cart(cart_products, df.antecedents)]
     df.sort_values('profitability', ascending=False)
@@ -27,4 +43,11 @@ def get_trending_products(df, cart_products, count=5):
 
 
 def is_not_in_cart(cart_products, suggestions):
+    """
+    Check if suggested product is already in the cart.
+
+    :param cart_products: list of products in the cart
+    :param suggestions: Pandas series of product suggestions
+    :return: Pandas series of booleans with same index as suggestions
+    """
     return [suggestion not in cart_products for suggestion in suggestions]
