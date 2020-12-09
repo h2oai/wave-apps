8833434c30fdf9f636043241d662f63ab838af54
Martin Turoci <martin.turoci@h2o.ai>
2020-12-09 11:05:45 +0100

    fix: discourage page drop in favor of simply changing items content

--- shopping-cart-recommendations/src/app.py
+++ shopping-cart-recommendations/src/app.py
@@ -7,7 +7,6 @@ from .utils import get_products_list, get_suggestions, get_trending_products
 
 
 def init_ui(q: Q):
-    q.page.drop()
     q.page['meta'] = ui.meta_card(box='', layouts=[
         ui.layout(
             breakpoint='xs',
@@ -53,7 +52,22 @@ def init_ui(q: Q):
         icon_color=config.icon_color,
     ))
 
-
+    q.page['suggestions'] = ui.form_card(box='suggestions', items=[])
+    q.page['trending'] = ui.form_card(box='trending', items=[])
+    q.page['cart'] = ui.form_card(
+        box='cart',
+        items=[
+            ui.separator('Cart'),
+            ui.text('Search and add products to the cart'),
+            ui.picker(
+                name='cart_products',
+                choices=[ui.choice(name=str(x), label=str(x)) for x in get_products_list()],
+                values=q.args.cart_products,
+                trigger=True,
+            ),
+        ]
+    )
+  
 def init_data(q: Q):
     q.client.cart_products = []  # Products in the initial cart
 
@@ -61,83 +75,45 @@ def init_data(q: Q):
     q.client.rule_set.consequents = q.client.rule_set.consequents.apply(lambda x: list(eval(x))[0])
 
 
-def render_cart(q: Q):
-    q.page['cart'] = ui.form_card(
-        box='cart',
-        items=[
-            ui.separator('Cart'),
-            ui.text('Search and add products to the cart'),
-            ui.picker(
-                name='cart_products_picker',
-                choices=[ui.choice(name=str(x), label=str(x)) for x in get_products_list()],
-                values=q.client.cart_products,
-                trigger=True,
-            ),
-        ]
-    )
-
-
 def render_suggestions(q: Q):
     suggestions = get_suggestions(q.client.rule_set, q.client.cart_products)
 
-    q.page['suggestions'] = ui.form_card(
-        box='suggestions',
-        items=[
-            ui.separator(label='Recommended for You'),
-            *[
-                ui.button(name='suggestion_btn', label=suggestion, value=suggestion, caption='Add to cart')
-                for suggestion in suggestions
-            ],
-        ]
-    )
+    q.page['suggestions'].items = [
+        ui.separator(label='Recommended for You'),
+        *[
+            ui.button(name='suggestion_btn', label=suggestion, value=suggestion, caption='Add to cart')
+            for suggestion in suggestions
+        ],
+    ]
 
 
 def render_trending(q: Q):
     trending_products = get_trending_products(q.client.rule_set, q.client.cart_products)
 
-    q.page['trending'] = ui.form_card(
-        box='trending',
-        items=[
-            ui.separator(label='Trending Now'),
-            *[
-                ui.button(name='trending_btn', label=product, value=product, caption='Add to cart')
-                for product in trending_products
-            ],
-        ]
-    )
-
-
-def handle_cart_products_change(q: Q):
-    q.client.cart_products = q.args.cart_products_picker
-
-
-def handle_suggestion_click(q: Q):
-    q.client.cart_products.append(q.args.suggestion_btn)
-
-
-def handle_trending_click(q: Q):
-    q.client.cart_products.append(q.args.trending_btn)
+    q.page['trending'].items = [
+        ui.separator(label='Trending Now'),
+        *[
+            ui.button(name='trending_btn', label=product, value=product, caption='Add to cart')
+            for product in trending_products
+        ],
+    ]
 
 
 @app('/')
 async def serve(q: Q):
-    q.page.drop()
     if not q.client.initialized:
         init_data(q)
+        init_ui(q)
         q.client.initialized = True
 
-    if q.args.cart_products_picker is not None:
-        handle_cart_products_change(q)
-
+    if q.args.cart_products is not None:
+        q.client.cart_products = q.args.cart_products
     if q.args.suggestion_btn:
-        handle_suggestion_click(q)
+        q.client.cart_products.append(q.args.suggestion_btn)
 
     if q.args.trending_btn:
-        handle_trending_click(q)
-
-    init_ui(q)
-    render_cart(q)
+        q.client.cart_products.append(q.args.trending_btn)
+        
     render_suggestions(q)
     render_trending(q)
-
     await q.page.save()
