89e24c90e45a1271d2bbbd0d48d1b9697005eeb9
Martin Turoci <martin.turoci@h2o.ai>
2021-03-15 14:51:02 +0100

    feat: redesign explaining ratings

--- explaining-ratings/src/app.py
+++ explaining-ratings/src/app.py
@@ -1,208 +1,143 @@
-from h2o_wave import main, app, Q, ui
-import json
-
+from h2o_wave import main, app, Q, ui, expando_to_dict
+from wordcloud import WordCloud, STOPWORDS
 from .config import Configuration
-from .utils.word_cloud_utils import plot_word_cloud, merge_to_single_text
-from .utils.data_utils import filter_data_frame
+
+import io
+import base64
+import matplotlib.pyplot as plt
 
 config = Configuration()
+word_cloud = WordCloud(mode='RGBA', background_color=None, stopwords=set(STOPWORDS), min_font_size=10)
 
 
-def render_home(q: Q):
-    q.page["left_panel"] = ui.form_card(box=config.boxes['left_panel'], items=[
-        ui.text_xl("Hotel Reviews"),
-        ui.dropdown(
-            name="review_choice",
-            label="Choose a review type",
-            placeholder=config.column_mapping[q.client.review] if q.client.review else "please select a review type",
-            choices=[
-                ui.choice(name=column, label=config.column_mapping[column])
-                for column in config.dataset[config.review_column_list]
-            ],
-            trigger=True,
-        ),
-    ])
+def plot_word_cloud(df, q: Q):
+    figsize = (6, 3)
+    width = 400
+    height = 200
+    if q.client.filters:
+        figsize = (6, 6)
+        width = 200
+        height = 400
+
+    word_cloud.height = height
+    word_cloud.width = width
+    word_cloud.generate(''.join(df))
+
+    fig = plt.figure(figsize=figsize)
+    plt.imshow(word_cloud)
+    plt.axis('off')
+    plt.tight_layout(pad=0)
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png', transparent=True)
+    buffer.seek(0)
+    plt.close(fig)
+
+    return base64.b64encode(buffer.read()).decode('utf-8')
 
 
-def populate_dropdown_list(q: Q):
-    filter_choices = [
-        ui.choice(
-            name=json.dumps({"id": q.client.filter_count + 1, "attr": column, "attr_val": None}),
-            label=config.column_mapping[column]
-        ) for column in config.filterable_columns
-    ]
-    items = [
-        ui.text_l("Filter reviews"),
-    ]
+def render_diff_word_cloud(q: Q):
+    df = config.dataset
 
     for key, value in q.client.filters.items():
-        for attr, attr_val in value.items():
-            items.append(ui.dropdown(
-                name="filter",
-                label="Choose a review attribute",
-                placeholder=config.column_mapping[attr],
-                choices=[
-                    ui.choice(
-                        name=json.dumps({'id': key, 'attr': column, 'attr_val': None}),
-                        label=config.column_mapping[column]
-                    ) for column in config.filterable_columns
-                ],
-                trigger=True,
-            ), )
-            items.append(ui.dropdown(
-                name="filter_value",
-                label="Choose a value for selected review attribute",
-                placeholder=str(attr_val),
-                choices=[ui.choice(name=json.dumps({'id': key, 'attr': attr, 'attr_val': column}), label=str(column)) for
-                         column in config.dataset[attr].drop_duplicates()],
-                trigger=True,
-            ), )
-            items.append(ui.separator())
-
-    if (q.args.add_filter and all(q.client.filters.values())) or q.args.review_choice or q.args.reset_filters:
-        items.append(ui.dropdown(
-            name="filter",
-            label="Choose a review attribute",
-            placeholder="Please select a category to filter",
-            choices=filter_choices,
-            trigger=True
-        ), )
-
-    items.append(ui.button(name="compare_review_button", label="Compare Reviews", primary=True))
-
-    return items
-
-
-def render_filter_toolbar(q):
-    render_home(q)
-
-    q.page["filter_toolbar"] = ui.toolbar_card(
-        box=config.boxes["new_filter"],
-        items=[
-            ui.command(
-                name='add_filter',
-                label='Add filter',
-                caption='Create a new filter',
-                icon='Add',
-            ),
-            ui.command(
-                name='reset_filters',
-                label='Reset filters',
-                caption='Reset the filters',
-                icon='Delete',
-            )
-        ]
-    )
-
-    filter_dropdown = populate_dropdown_list(q)
-
-    q.page["filters"] = ui.form_card(box=config.boxes['filters'], items=filter_dropdown)
-
-
-def get_text_word_cloud_plot(q: Q):
-    return plot_word_cloud(merge_to_single_text(config.dataset[q.client.review]))
-
-
-def render_text_word_cloud_image(q: Q, image):
-    q.page['all'] = ui.image_card(
-        box=config.boxes['middle_panel'],
-        title=f'Word Cloud of the {config.column_mapping[q.client.review]}',
-        type='png',
-        image=image,
-    )
-
-
-def render_all_text_word_cloud(q: Q):
-    image = plot_word_cloud(merge_to_single_text(config.dataset[q.client.review]))
-
-    q.page['all'] = ui.image_card(
-        box=config.boxes['middle_panel'],
-        title=f'Word Cloud of the {config.column_mapping[q.client.review]}',
-        type='png',
-        image=image,
-    )
-
-
-def render_compare_word_cloud(q: Q):
-    df = filter_data_frame(config.dataset, q.client.filters)
+        df = df[df[key] == value]
 
     if len(df):
-        image = plot_word_cloud(merge_to_single_text(df[q.client.review]))
-
-        q.page['compare'] = ui.image_card(
-            box=config.boxes['right_panel'],
-            title='Word Cloud based on the selected filters',
-            type='png',
-            image=image,
-        )
+        # Remove when https://github.com/h2oai/wave/pull/611 merged.
+        q.page['meta'].layouts[0].zones[1].zones[1].zones[1].size = '1'
+        q.page['diff'] = ui.image_card(box='diff', title='Diff', type='png', image=plot_word_cloud(df[q.client.review], q))
     else:
-        q.page['compare'] = ui.form_card(
-            box=config.boxes['right_panel'],
-            items=[
-                ui.message_bar(type='warning', text='No reviews matching filter criteria!')
+        # TODO: Move into sidebar when https://github.com/h2oai/wave/pull/507 merged.
+        q.page['diff'] = ui.form_card(box='diff', items=[ui.message_bar(type='warning', text='No reviews matching filter criteria!')])
+
+
+def init(q: Q):
+    q.page['meta'] = ui.meta_card(box='', title='Explain Ratings', layouts=[
+        ui.layout(
+            breakpoint='xs',
+            zones=[
+                ui.zone('header'),
+                ui.zone('body', direction=ui.ZoneDirection.ROW, size='calc(100vh - 70px)', zones=[
+                    ui.zone('sidebar', size='350px'),
+                    ui.zone('content', direction=ui.ZoneDirection.ROW, zones=[
+                        ui.zone('all'),
+                        ui.zone('diff')
+                    ])
+                ])
             ]
-        )
+        ),
+    ])
+    q.page['header'] = ui.header_card(box='header', title='Hotel Reviews', subtitle='Explains the hotel reviews', icon='ReviewSolid', icon_color='#00A8E0')
+    q.client.review = config.review_column_list[0]
+
+    form_filters = []
+    for column in config.filterable_columns:
+        choices = [ui.choice(name='empty', label='All')] + [ui.choice(name=str(column), label=str(column)) for column in config.dataset[column].drop_duplicates()]
+        form_filters.append(ui.dropdown(name=f'filter_{column}', label=config.column_mapping[column], trigger=True, value='empty', choices=choices))
+
+    sidebar_items = [ui.dropdown(name='review', label='Review type', value=q.client.review, trigger=True,
+        choices=[ui.choice(name=column, label=config.column_mapping[column]) for column in config.dataset[config.review_column_list]]
+    ),
+    ui.separator('Filters')] + form_filters
+    q.page['sidebar'] = ui.form_card(box='sidebar', items=sidebar_items)
+    q.page['original'] = ui.image_card(box='all', title='Original', type='png', image=plot_word_cloud(config.dataset[q.client.review], q))
 
 
-async def init(q: Q):
-    if not q.client.app_initialized:
-        (q.app.header_png,) = await q.site.upload([config.image_path])
-        (q.app.training_file_url,) = await q.site.upload([config.training_path])
-        reset_filters(q)
-        config.init_dataset()
-        q.client.app_initialized = True
-
-    q.page.drop()
-
-    q.page["title"] = ui.header_card(
-        box=config.boxes["banner"],
-        title=config.title,
-        subtitle=config.subtitle,
-        icon=config.icon,
-        icon_color=config.color,
-    )
-
-
-def reset_filters(q: Q):
-    q.client.filter_count = 0
-    q.client.filters = {}
-
-
-@app("/")
-async def serve(q: Q):
-    await init(q)
-    if q.args.review_choice:
-        q.client.review = q.args.review_choice
-        render_filter_toolbar(q)
-        q.client.all_text_word_cloud = get_text_word_cloud_plot(q)
-        render_text_word_cloud_image(q, q.client.all_text_word_cloud)
-    elif q.args.add_filter:
-        q.client.filter_count += 1
-        if not q.client.filters:
-            q.client.filters = {}
-        if q.args.filter:
-            q.client.filters[q.args.filter] = None
-        render_filter_toolbar(q)
-        render_text_word_cloud_image(q, q.client.all_text_word_cloud)
-    elif q.args.filter_value:
-        q.args.filter_value = json.loads(q.args.filter_value)
-        q.client.filters[q.args.filter_value['id']] = {q.args.filter_value['attr']: q.args.filter_value['attr_val']}
-        render_filter_toolbar(q)
-        render_text_word_cloud_image(q, q.client.all_text_word_cloud)
-    elif q.args.filter:
-        q.args.filter = json.loads(q.args.filter)
-        q.client.filters[q.args.filter['id']] = {q.args.filter['attr']: q.args.filter['attr_val']}
-        render_filter_toolbar(q)
-        render_text_word_cloud_image(q, q.client.all_text_word_cloud)
-    elif q.args.reset_filters:
-        reset_filters(q)
-        render_filter_toolbar(q)
-        render_all_text_word_cloud(q)
-    elif q.args.compare_review_button:
-        render_filter_toolbar(q)
-
-        render_text_word_cloud_image(q, q.client.all_text_word_cloud)
-        render_compare_word_cloud(q)
+def handle_filter(q: Q, key: str, val: str):
+    if val == 'empty':
+        q.client.filters.pop(key, None)
     else:
-        render_home(q)
+        q.client.filters[key] = val
+
+
+@app('/')
+async def serve(q: Q):
+    if not q.client.initialized:
+        init(q)
+        q.client.filters = {}
+        q.client.initialized = True
+
+    args = expando_to_dict(q.args)
+    for arg in args:
+        if str(arg).startswith('filter_'):
+            handle_filter(q, str(arg).replace('filter_', ''), q.args[arg])
+
+    sidebar_items = q.page['sidebar'].items
+    if q.args.filter_categories:
+        # TODO: Remove after https://github.com/h2oai/wave/issues/150 gets resolved.
+        sidebar_items[2].dropdown.value = q.args.filter_categories
+    if q.args.filter_city:
+        # TODO: Remove after https://github.com/h2oai/wave/issues/150 gets resolved.
+        sidebar_items[3].dropdown.value = q.args.filter_city
+    if q.args.filter_country:
+        # TODO: Remove after https://github.com/h2oai/wave/issues/150 gets resolved.
+        sidebar_items[4].dropdown.value = q.args.filter_country
+    if q.args.filter_postalCode:
+        # TODO: Remove after https://github.com/h2oai/wave/issues/150 gets resolved.
+        sidebar_items[5].dropdown.value = q.args.filter_postalCode
+    if q.args.filter_province:
+        # TODO: Remove after https://github.com/h2oai/wave/issues/150 gets resolved.
+        sidebar_items[6].dropdown.value = q.args.filter_province
+    if q.args.filter_rating:
+        # TODO: Remove after https://github.com/h2oai/wave/issues/150 gets resolved.
+        sidebar_items[7].dropdown.value = q.args.filter_rating
+    if q.args.filter_userCity:
+        # TODO: Remove after https://github.com/h2oai/wave/issues/150 gets resolved.
+        sidebar_items[8].dropdown.value = q.args.filter_userCity
+    if q.args.filter_userProvince:
+        # TODO: Remove after https://github.com/h2oai/wave/issues/150 gets resolved.
+        sidebar_items[9].dropdown.value = q.args.filter_userProvince
+    if q.args.review:
+        q.client.review = q.args.review
+        # TODO: Remove after https://github.com/h2oai/wave/issues/150 gets resolved.
+        sidebar_items[0].dropdown.value = q.client.review
+        q.page['original'].image = plot_word_cloud(config.dataset[q.client.review], q)
+
+    if q.client.filters:
+        render_diff_word_cloud(q)
+    else:
+        del q.page['diff']
+        # TODO: Remove when https://github.com/h2oai/wave/pull/611 merged.
+        q.page['meta'].layouts[0].zones[1].zones[1].zones[1].size = '0'
+
     await q.page.save()
--- explaining-ratings/src/utils/data_utils.py
+++
@@ -1,6 +0,0 @@
-def filter_data_frame(df, filters_map):
-    for key, value in filters_map.items():
-        for attr, attr_value in value.items():
-            df = df[df[attr] == attr_value]
-
-    return df
