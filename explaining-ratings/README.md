fc4b007265e3825288a791e1b6236c1ff8180184
mturoci <64769322+mturoci@users.noreply.github.com>
2021-04-20 15:34:01 +0200

    feat: redesign explaining ratings (#47)
    * feat: redesign explaining ratings
    
    * feat: use a single zone for content instead of 2

--- explaining-ratings/README.md
+++ explaining-ratings/README.md
@@ -1,26 +1,27 @@
 # Explain Text Reviews Wave App
 
-This application allows users to build word clouds on the hotel reviews texts. It further allows the users to filter reviews and compare the word clouds. 
+This application allows users to build word clouds on the hotel reviews texts. It further allows the users to filter reviews and compare the word clouds.
 
 ![Explaining Ratings App Screenshot](docs/screenshots/explain-text-app.png)
 
-
 ## Running this App Locally
 
-### System Requirements 
+### System Requirements
+
 1. Python 3.6+
 2. pip3
 
 ### 1. Run the Wave Server
-New to H2O Wave? We recommend starting in the documentation to [download and run](https://h2oai.github.io/wave/docs/installation) the Wave Server on your local machine. Once the server is up and running you can easily use any Wave app. 
+
+New to H2O Wave? We recommend starting in the documentation to [download and run](https://h2oai.github.io/wave/docs/installation) the Wave Server on your local machine. Once the server is up and running you can easily use any Wave app.
 
 ### 2. Setup Your Python Environment
 
 ```bash
-$ git clone git@github.com:h2oai/wave-apps.git
-$ cd wave-apps/explaining-ratings
-$ make setup
-$ source venv/bin/activate
+git clone git@github.com:h2oai/wave-apps.git
+cd wave-apps/explaining-ratings
+make setup
+source venv/bin/activate
 ```
 
 ### 3. Run the App
@@ -30,9 +31,11 @@ wave run src.app
 ```
 
 Note! If you did not activate your virtual environment this will be:
+
 ```bash
 ./venv/bin/wave run src.app
 ```
 
 ### 4. View the App
+
 Point your favorite web browser to [localhost:10101](http://localhost:10101)
--- explaining-ratings/src/config.py
+++ explaining-ratings/src/config.py
@@ -2,49 +2,22 @@ import pandas as pd
 
 
 class Configuration:
-    """
-    Configuration file for Explain Ratings
-    """
-
     def __init__(self):
-        self.color = "#00A8E0"
-        self.image_path = "static/icon.png"
-        self.title = "Hotel Reviews"
-        self.subtitle = "Explains the hotel reviews"
-        self.icon = "ReviewSolid"
         self.review_column_list = ['reviews.title', 'reviews.text']
-        self.training_path = "data/Hotel_Reviews.csv"
-        self.default_model = "explain_rating_model"
-
-        self.dataset = None
-        self.filterable_columns = ['categories', 'city', 'country', 'postalCode', 'province',
-                                   'reviews.rating', 'reviews.userCity', 'reviews.userProvince']
+        self.filterable_columns = ['categories', 'city', 'country', 'postalCode', 'province', 'rating', 'userCity', 'userProvince']
         self.column_mapping = {
-            'reviews.title': 'Review Title',
-            'reviews.text': 'Review Description',
+            'reviews.title': 'Title',
+            'reviews.text': 'Description',
             'categories': 'Categories',
             'city': 'City',
             'country': 'Country',
             'postalCode': 'Postal Code',
             'province': 'Province',
-            'reviews.rating': 'Rating',
-            'reviews.userCity': 'Reviewer City',
-            'reviews.userProvince': 'Reviewer Province',
+            'rating': 'Rating',
+            'userCity': 'Reviewer City',
+            'userProvince': 'Reviewer Province',
         }
-
-        self.boxes = {
-            "banner": "1 1 12 1",
-            "content": "1 2 -1 -1",
-            "left_panel": "1 2 3 2",
-            "new_filter": "1 4 3 1",
-            "filters": "1 5 3 -1",
-            "middle_panel": "4 2 4 -1",
-            "right_panel": "8 2 5 -1",
-        }
-
-    def init_dataset(self, refresh=False):
-        if refresh or self.dataset is None:
-            df = pd.read_csv(self.training_path).head(50)
-            df.dropna(subset=self.filterable_columns, inplace=True)
-            df['reviews.rating'] = df['reviews.rating'].astype(int)
-            self.dataset = df
+        df = pd.read_csv('data/Hotel_Reviews.csv').head(50)
+        df.dropna(subset=self.filterable_columns, inplace=True)
+        df['rating'] = df['rating'].astype(int)
+        self.dataset = df
