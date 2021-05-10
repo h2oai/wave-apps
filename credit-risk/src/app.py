2b541f1edc7a8318c32ea8bb7fd42724059e0506
Joe G <joseph.granados@h2o.ai>
2021-05-09 19:12:44 -0700

    Credit Risk app update

--- credit-risk/src/app.py
+++ credit-risk/src/app.py
@@ -1,28 +1,234 @@
-from h2o_wave import app, Q, ui, main
+import pandas as pd
+from h2o_wave import app, data, main, Q, ui
 
-from .views.home import render_home
-from .views.customer import render_customer_page, handle_approve_click, handle_reject_click
+from .model import Model
+
+
+TRAIN_CSV = "./data/Kaggle/CreditCard-train.csv"
+TEST_CSV = "./data/Kaggle/CreditCard-train.csv"
+ID_COLUMN = "ID"
+TARGET_COLUMN = "default.payment.next.month"
+# Recommend rejecting customer's with default probability appove this amount 
+APPROVAL_THRESHOLD = 0.35
 
 
 def init(q: Q):
-    q.app.customer_status = {}
+    # build model
+    q.app.model = Model(TRAIN_CSV, ID_COLUMN, TARGET_COLUMN)
+    # prep df for analyzation
+    df = pd.read_csv(TEST_CSV).head(20)
+    df.drop(TARGET_COLUMN, axis=1, inplace=True)
+    # analyze df
+    q.app.predictions_df = q.app.model.predict(df)
+    q.app.contributions_df = q.app.model.contrib(df)
+    # prep df for viewing
+    df['Default Prediction Rate'] = q.app.predictions_df.round(4)
+    df.insert(loc=0, column='Status', value='Pending', allow_duplicates=True) 
+    q.app.customer_df = df
+
+
+def layout(q: Q):
+    q.page['meta'] = ui.meta_card(
+        box='', 
+        title='Credit Risk', 
+        layouts=[
+            ui.layout(
+                breakpoint='xs',
+                zones=[
+                    ui.zone('header'),
+                    ui.zone('customer_table'),
+                    ui.zone(
+                        'customer_page',
+                        zones=[
+                            ui.zone('customer_risk_explanation'),
+                            ui.zone('customer_shap_plot', size='600px'),
+                            ui.zone('button_group'),
+                            ui.zone('customer_features'),
+                        ]
+                    )
+                ]
+            ),
+            ui.layout(
+                breakpoint='m',
+                zones=[
+                    ui.zone('header'),
+                    ui.zone('customer_table'),
+                    ui.zone(
+                        'customer_page',
+                        direction=ui.ZoneDirection.ROW,
+                        zones=[
+                            ui.zone(
+                                'content',
+                                zones=[
+                                    ui.zone('customer_risk_explanation'),
+                                    ui.zone('customer_shap_plot', size='600px'),
+                                    ui.zone('button_group'),
+                                ]
+                            ),
+                            ui.zone('customer_features', size='300px'),
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+    q.page['header'] = ui.header_card(
+        box='header',
+        title='Credit Card Risk',
+        subtitle='Review customer ability to pay credit card bills',
+        icon='PaymentCard',
+        nav=[
+            ui.nav_group(
+                'Navigation', 
+                items=[
+                    ui.nav_item(name='render_customer_selector', label='Customers'),
+                ]
+            ),
+            ui.nav_group(
+                'Options', 
+                items=[
+                    ui.nav_item(name='dark_mode', label='Dark Mode'),
+                    ui.nav_item(name='light_mode', label='Light Mode'),
+                ]
+            )
+        ]
+    )
+
+
+def handle_approve_click(q: Q):
+    q.app.customer_df.loc[q.app.customer_df[ID_COLUMN] == q.client.selected_customer_id, 'Status'] = 'Approved'
+
+
+def handle_reject_click(q: Q):
+    q.app.customer_df.loc[q.app.customer_df[ID_COLUMN] == q.client.selected_customer_id, 'Status'] = 'Rejected'
+
+
+async def render_customer_page(q: Q):
+
+    del q.page['customer_table']
+
+    row_index = int(q.args.customer_table[0])
+    customer_row = q.app.customer_df.loc[row_index]
+    score = q.app.predictions_df.loc[row_index]
+    approve = bool(score < APPROVAL_THRESHOLD)
+    contribs = q.app.contributions_df.loc[row_index].drop("BiasTerm")
+
+    q.client.selected_customer_id = customer_row["ID"]
+
+    # details
+    q.page.add(
+        'customer_features', 
+        ui.form_card(
+            box='customer_features',
+            items=[
+                ui.table(
+                    name='customer_features',
+                    columns=[
+                        ui.table_column(name="attribute", label="Attribute", sortable=False, searchable=False, max_width='100'),
+                        ui.table_column(name="value", label="Value", sortable=False, searchable=False, max_width='100')
+                    ],
+                    rows=[ui.table_row(name=index, cells=[index, row]) for index, row in customer_row.map(str).iteritems()],
+                    groupable=False,
+                    resettable=False,
+                    multiple=False,
+                    height='760px'
+                )
+            ]
+        )
+    )
+
+    # summary
+    top_feature = contribs.idxmin(axis=1) if approve else contribs.idxmax(axis=1)
+    explanation_data = {
+        'will_or_will_not': 'will' if approve else 'will not',
+        'top_contributing_feature': top_feature,
+        'value_of_top_contributing_feature': str(customer_row[top_feature]),
+        'accept_or_reject': 'approve' if approve else 'reject',
+    }
+    explanation = (
+        "- This customer **{{will_or_will_not}}** most probably settle the next month credit card balance.\n"
+        "- Having a **{{top_contributing_feature}}** of **{{value_of_top_contributing_feature}}** is the top reason for that.\n"
+        "- It's a good idea to **{{accept_or_reject}}** this customer." 
+    )
+    q.page["customer_risk_explanation"] = ui.markdown_card(
+        box='customer_risk_explanation',
+        title='Summary on Customer',
+        content='=' + explanation,
+        data=explanation_data,
+    )
+
+    # shap plot
+    shap_values = list(zip(contribs.index, contribs))
+    shap_values.sort(key=lambda x: x[1])
+    q.page['customer_shap_plot'] = ui.plot_card(
+        box='customer_shap_plot',
+        title="Effectiveness of each attribute on defaulting next payment",
+        data=data(['label', 'value'], rows=shap_values),
+        plot=ui.plot([ui.mark(type='interval', x='=value', x_title='SHAP value', y='=label')])
+    )
+
+    # approve/reject buttons
+    q.page["button_group"] = ui.form_card(
+        box='button_group',
+        items=[
+            ui.buttons(
+                [
+                    ui.button(name='approve_btn', label='Approve', primary=approve),
+                    ui.button(name='reject_btn', label='Reject', primary=not approve),
+                ]
+            )
+        ]
+    )
+
+    await q.page.save()
+
+
+async def render_customer_selector(q: Q):
+
+    del q.page['customer_features']
+    del q.page['customer_risk_explanation']
+    del q.page['customer_shap_plot']
+    del q.page['button_group']
+
+    columns = [ui.table_column(name=column, label=column, sortable=True, searchable=True) for column in q.app.customer_df.columns]
+    rows = [ui.table_row(name=str(index), cells=row.tolist()) for index, row in q.app.customer_df.applymap(str).iterrows()]
+    q.page["customer_table"] = ui.form_card(
+        box='customer_table',
+        items=[
+            ui.message_bar(text='Click "Status" to review a customer', type='info'),
+            ui.table(
+                name='customer_table',
+                columns=columns,
+                rows=rows,
+                groupable=True,
+                multiple=False,
+            )
+        ]
+    )
+
+    await q.page.save()
 
 
 @app("/")
 async def serve(q: Q):
     if not q.app.initialized:
+        layout(q)
         init(q)
         q.app.initialized = True
 
-    if q.args.risk_table:
-        render_customer_page(q)
+    if q.args.dark_mode:
+        q.page['meta'].theme = 'neon'
+        await q.page.save()
+    elif q.args.light_mode:
+        q.page['meta'].theme = 'default'
+        await q.page.save()
+    elif q.args.customer_table:
+        await render_customer_page(q)
     elif q.args.approve_btn:
         handle_approve_click(q)
-        render_home(q)
+        await render_customer_selector(q)
     elif q.args.reject_btn:
         handle_reject_click(q)
-        render_home(q)
+        await render_customer_selector(q)
     else:
-        render_home(q)
-
-    await q.page.save()
+        await render_customer_selector(q)
