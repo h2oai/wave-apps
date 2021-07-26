22ca5084bf0fc58c83efbc7189000b4c0d1efe5c
Dulaj Atapattu <dulaj.atapattu@h2o.ai>
2021-07-26 11:39:56 +0530

    Remove app code page

--- churn-risk/src/app.py
+++ churn-risk/src/app.py
@@ -1,19 +1,14 @@
 from typing import List, Optional
 import pandas as pd
-import os
 
-from pygments import highlight
-from pygments.formatters.html import HtmlFormatter
-from pygments.lexers import get_lexer_by_name
 from h2o_wave import app, main, Q, ui, data
 from .churn_predictor import ChurnPredictor
 
-
-TRAIN_DATASET_PATH='./data/churnTrain.csv'
-TEST_DATASET_PATH='./data/churnTest.csv'
-TARGET_COLUMN="Churn?"
-CATEGORICAL_COLUMNS=['Area Code']
-DROP_COLUMNS=["Phone"]
+TRAIN_DATASET_PATH = './data/churnTrain.csv'
+TEST_DATASET_PATH = './data/churnTest.csv'
+TARGET_COLUMN = "Churn?"
+CATEGORICAL_COLUMNS = ['Area Code']
+DROP_COLUMNS = ["Phone"]
 
 df = pd.read_csv(TEST_DATASET_PATH)
 df.dropna(inplace=True)
@@ -33,16 +28,20 @@ def render_shap_plot(q: Q, shap_rows: List, selected_row_index: Optional[int]):
         box=ui.box('top-plot', height='700px'),
         title='Shap explanation' if selected_row_index else 'Global Shap',
         data=data(['label', 'value'], rows=shap_rows),
-        plot=ui.plot([ui.mark(type='interval', x='=value', x_title='SHAP value', y='=label', color=q.client.secondary_color)])
+        plot=ui.plot(
+            [ui.mark(type='interval', x='=value', x_title='SHAP value', y='=label', color=q.client.secondary_color)])
     )
 
 
 def render_negative_pdp_plot(q: Q, shap_rows: List, selected_row_index: Optional[int]):
     min_contrib_col = shap_rows[-1][0] if selected_row_index is None else None
-    is_cat, min_contrib_col, retention_rows = churn_predictor.get_negative_explanation(selected_row_index, min_contrib_col)
+    is_cat, min_contrib_col, retention_rows = churn_predictor.get_negative_explanation(selected_row_index,
+                                                                                       min_contrib_col)
     plot = [
-        ui.mark(type='interval', x='=label', y='=size', x_title=min_contrib_col, color=q.client.secondary_color, fill_opacity=0.5),
-        ui.mark(type='line' if is_cat else 'point', x='=label', y='=value', color=q.client.primary_color, shape='circle'),
+        ui.mark(type='interval', x='=label', y='=size', x_title=min_contrib_col, color=q.client.secondary_color,
+                fill_opacity=0.5),
+        ui.mark(type='line' if is_cat else 'point', x='=label', y='=value', color=q.client.primary_color,
+                shape='circle'),
     ]
     if selected_row_index is not None:
         plot.append(ui.mark(x=churn_predictor.get_python_type(df[min_contrib_col][selected_row_index])))
@@ -58,8 +57,10 @@ def render_positive_pdp_plot(q: Q, shap_rows: List, selected_row_index: Optional
     max_contrib_col = shap_rows[0][0] if selected_row_index is None else None
     is_cat, max_contrib_col, churn_rows = churn_predictor.get_positive_explanation(selected_row_index, max_contrib_col)
     plot = [
-        ui.mark(type='interval', x='=label', y='=size', x_title=max_contrib_col, color=q.client.secondary_color, fill_opacity=0.5),
-        ui.mark(type='line' if is_cat else 'point', x='=label', y='=value', color=q.client.primary_color, shape='circle'),
+        ui.mark(type='interval', x='=label', y='=size', x_title=max_contrib_col, color=q.client.secondary_color,
+                fill_opacity=0.5),
+        ui.mark(type='line' if is_cat else 'point', x='=label', y='=value', color=q.client.primary_color,
+                shape='circle'),
     ]
     if selected_row_index is not None:
         plot.append(ui.mark(x=churn_predictor.get_python_type(df[max_contrib_col][selected_row_index])))
@@ -85,7 +86,8 @@ def render_desc_info(q: Q, selected_row_index: Optional[int]):
 
     total_charges = df['Total Charges']
     charge = total_charges[selected_row_index] if selected_row_index is not None else total_charges.mean(axis=0)
-    rank = df['Total Charges'].rank(pct=True).values[selected_row_index] if selected_row_index is not None else df['Total Charges'].rank(pct=True).mean(axis=0)
+    rank = df['Total Charges'].rank(pct=True).values[selected_row_index] if selected_row_index is not None else df[
+        'Total Charges'].rank(pct=True).mean(axis=0)
     q.page['total_charges'] = ui.tall_gauge_stat_card(
         box='top-stats',
         title='Total Charges' if selected_row_index else 'Average Total Charges',
@@ -114,7 +116,23 @@ def render_charges_breakdown(q: Q, selected_row_index: Optional[int]):
     )
 
 
-def render_analysis(q: Q):
+def render_analysis_page(q: Q):
+    q.page['title'] = ui.section_card(
+        box='title',
+        title='Customer profiles from model predictions',
+        subtitle='Customer: No customer chosen',
+        items=[
+            ui.picker(
+                name='customers',
+                label='Customer Phone Number',
+                choices=[ui.choice(name=str(phone), label=str(phone)) for phone in df['Phone']],
+                max_choices=1,
+                trigger=True
+            ),
+            ui.toggle(name='theme', label='Dark Theme', trigger=True)
+        ]
+    )
+
     row_phone_no = int(q.args.customers[0]) if q.args.customers else None
     q.page['title'].items[0].picker.values = q.args.customers
     q.page['title'].subtitle = f'Customer: {row_phone_no or "No customer selected"}'
@@ -128,14 +146,14 @@ def render_analysis(q: Q):
     render_charges_breakdown(q, selected_row_index)
 
 
-def render_code(q: Q):
-    local_dir = os.path.dirname(os.path.realpath(__file__))
-    with open(os.path.join(local_dir, 'app.py')) as f:
-        contents = f.read()
-
-    py_lexer = get_lexer_by_name("python")
-    html_formatter = HtmlFormatter(full=True, style="xcode")
-    q.page['code'] = ui.frame_card(box=ui.box('code', height='calc(100vh - 155px)'), title='', content=highlight(contents, py_lexer, html_formatter))
+def render_my_page(q: Q):
+    q.page['my_page'] = ui.form_card(
+        box=ui.box('my_page', height='calc(100vh - 155px)'),
+        title='This is my page',
+        items=[
+            ui.text('Hello World!')
+        ]
+    )
 
 
 def init(q: Q):
@@ -147,7 +165,7 @@ def init(q: Q):
             ui.zone('header'),
             ui.zone('title'),
             ui.zone('content', zones=[
-                ui.zone('code'),
+                ui.zone('my_page'),
                 ui.zone('top', direction=ui.ZoneDirection.ROW, zones=[
                     ui.zone('top-plot', size='70%'),
                     ui.zone('top-stats')
@@ -163,24 +181,8 @@ def init(q: Q):
         nav=[
             ui.nav_group('Main Menu', items=[
                 ui.nav_item(name='analysis', label='Analysis'),
-                ui.nav_item(name='code', label='Application Code'),
+                ui.nav_item(name='my_page', label='My Page'),
             ])
-        ]
-    )
-    q.page['title'] = ui.section_card(
-        box='title',
-        title='Customer profiles from model predictions',
-        subtitle='Customer: No customer chosen',
-        items=[
-            # TODO: Replace with dropdown after https://github.com/h2oai/wave/pull/303 merged.
-            ui.picker(
-                name='customers',
-                label='Customer Phone Number',
-                choices=[ui.choice(name=str(phone), label=str(phone)) for phone in df['Phone']],
-                max_choices=1,
-                trigger=True
-            ),
-            ui.toggle(name='theme', label='Dark Theme', trigger=True)
         ]
     )
 
@@ -205,16 +207,17 @@ async def serve(q: Q):
             q.client.tertiary_color = '$azure'
         q.page['title'].items[1].toggle.value = dark_theme
 
-    if q.args.code:
+    if q.args.my_page:
+        del q.page['title']
         del q.page['shap_plot']
         del q.page['top_negative_plot']
         del q.page['top_positive_plot']
         del q.page['total_charges']
         del q.page['bar_chart']
         del q.page['churn_rate']
-        render_code(q)
+        render_my_page(q)
     else:
-        del q.page['code']
-        render_analysis(q)
+        del q.page['my_page']
+        render_analysis_page(q)
 
     await q.page.save()
