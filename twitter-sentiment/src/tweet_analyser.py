452b607a9d108479c986ce1c67e4b84f4d6303b5
mturoci <64769322+mturoci@users.noreply.github.com>
2022-10-03 15:12:29 +0200

    feat: Simplify the code (#89)

--- twitter-sentiment/src/tweet_analyser.py
+++ twitter-sentiment/src/tweet_analyser.py
@@ -3,6 +3,11 @@ from tweepy.api import API
 from tweepy.auth import OAuthHandler
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+ACCESS_TOKEN = '2944447934-6KB33SzWsywpjYuibVLlKDFldCbYq0OcP7jJRg2'
+ACCESS_TOKEN_SECRET = 'teBNYDJovG8lNrwUk8w2TWErR7gEaGRHTdI105dcw0yV5'
+CONSUMER_KEY = 'TwgJUSNeivHHOJXLj9dUQzMOp'
+CONSUMER_SECRET = 'cXHG8F298S5SYr0wVobZqZLH7qmOGyPghYGbH9Q6ZVb2KuchIr'
+
 
 class TweetAnalyser:
     """
@@ -13,10 +18,10 @@ class TweetAnalyser:
         with a minimal change to the app code.
         """
 
-    def __init__(self, access_token, access_token_secret, consumer_key, consumer_secret):
+    def __init__(self):
         self.analyser = SentimentIntensityAnalyzer()
-        self.auth_handler = OAuthHandler(consumer_key, consumer_secret)
-        self.auth_handler.set_access_token(access_token, access_token_secret)
+        self.auth_handler = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        self.auth_handler.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         self.api = API(self.auth_handler, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
     def search_tweets(self, q: str, lang='en', rpp=100, items=12):
